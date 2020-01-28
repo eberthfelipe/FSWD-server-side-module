73937ff2b346ca8e820ae75c74afe3928e547f67
Eberth Felipe Castro da Cruz <eberth.c@samsung.com>
2020-01-28 12:46:18 -0400

    Cors: part 1

--- conFusionServer/routes/leaderRouter.js
+++ conFusionServer/routes/leaderRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const authenticate = require('../authenticate');
+const cors = require('./cors');
 const Leaders = require('../models/leaders');
 
 const leaderRouter = express.Router();
