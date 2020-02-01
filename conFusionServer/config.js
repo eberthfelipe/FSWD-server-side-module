96757cdede2fd2721e0004e48629b4f706400ab5
EberthFelipe <eberthfelipe@gmail.com>
2020-02-01 00:19:15 -0400

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '188264512550726',
+        clientSecret: '54702e705a51fb3c66ec7ecf5c30d289'
+    }
 }
